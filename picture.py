52d9d03f07c089cc0e97c339ea1d8a758b99ddb7
wasiwala <50227444+wala481652@users.noreply.github.com>
2021-08-20 17:21:22 +0800

    1100820 更新
    主要是將程式碼模組化

--- picture.py
+++ picture.py
@@ -1,3 +1,4 @@
+import re
 import numpy as np
 import os
 from tensorflow.keras.models import load_model
@@ -5,25 +6,31 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet import preprocess_input
 # from tensorflow.keras.callbacks import CSVLogger
 
-csv_path = './database/CSV/TCGA-CH-5744-01A-01-TS1.csv'
-# patch_path = './database/PNG/TCGA-CH-5740-01A-01-BS1/TCGA-CH-5740-01A-01-BS1_0_0.png'
-patch_path = './database/PNG/PRAD.1-ERG/TCGA-CH-5744-01A-01-TS1/'
-patch_file = os.listdir(patch_path)
+model_path = 'model_resnet50'
+csv_path = './database/CSV/TCGA-CH-5740-01A-01-BS1_02.csv'
+patch_path = './database/PNG/TCGA-CH-5740-01A-01-BS1/TCGA-CH-5740-01A-01-BS1_0_0.png'
+patch_directory = './database/PNG/PRAD.1-ERG/TCGA-CH-5740-01A-01-BS1/'
+patch_list = os.listdir(patch_directory)
 table = []
 
-model = load_model('./1100802_model_resnet50')
 
-for i in range(len(patch_file)):
-    img = image.load_img(patch_path + patch_file[i])
-    # img = image.load_img(patch_path)
+def picture(patch_path):
+    model = load_model(model_path)
+
+    img = image.load_img(patch_path)
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
     preds = model.predict(x)
-    a = preds.reshape(2)
-    print('Predicted: ', a)
+    return preds
 
-    table.append(a[0])
 
-np.savetxt(csv_path, table, delimiter=",")
+if __name__ == '__main__':
+
+    for i in range(len(patch_list)):
+        preds = picture(patch_directory + patch_list[i])
+        a = preds.reshape(2)
+        table.append(a[1])
+
+    np.savetxt(csv_path, table, delimiter=",")
